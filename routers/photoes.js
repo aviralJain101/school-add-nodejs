68ea56bad9b105763630e25751981deb335bf28c
aviralJain101 <aviraljain.ece18@itbhu.ac.in>
2020-11-02 16:22:55 +0530

    updated

--- routers/photoes.js
+++ routers/photoes.js
@@ -9,5 +9,41 @@ const authUser = require('../middleware/auth_user');
 
 const router = new express.Router();
 
+//post for school
+router.post('/photo/school', authSchool.schoolAuth, async(req, res) => {
+    const school = req.school;
+    try{
+        const photo = await Photo.create({
+            by: 0,
+            schoolName: school.name,
+            schoolId: school.id,
+            photoUrl: req.body.photoUrl
+        })
+        res.send(photo.photoUrl);
+    }catch(e){
+        res.status(440).send(e);
+    }
+})
+
+//get all photo from School
+router.get('/photo/school', authSchool.schoolAuth, async(req, res) => {
+    const school = req.school;
+    try{
+        const photo = await Photo.findAll({
+            where: {
+                schoolId: school.id,
+                by: 0
+            }
+        })
+        console.log(Array.isArray(photo));
+        var urls = [];
+        for(var i=0;i<photo.length;i++){
+            urls.push(photo[i].photoUrl);
+        }
+        res.send(urls);
+    }catch(e){
+        res.status(404).send(e);
+    }
+})
 
 module.exports = router;
