2b0117e3bf91ba960398fcf6b803e7f2a47f35fb
aviralJain101 <aviraljain.ece18@itbhu.ac.in>
2020-11-03 10:50:33 +0530

    update

--- routers/photoes.js
+++ routers/photoes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 
 const School = require('../models/school');
-const Schooldetail = require('../models/school_details');
 const User = require('../models/user');
 const Photo = require('../models/photoes');
 const authSchool = require('../middleware/auth_school');
